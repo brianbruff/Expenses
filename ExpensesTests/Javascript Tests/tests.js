19157a8939c5a9ef9af90d3f610a261283b5ecff
Brian Keating <b@briankeating.net>
2013-03-27 21:45:05 +0000

    Add qUnit infrastructure

---
+++ ExpensesTests/Javascript Tests/tests.js
@@ -0,0 +1,15 @@
+﻿/// <reference path="../../Expenses.Web/Scripts/app/expenses.viewmodel.js" />
+
+
+
+test("hello test", function () {
+    // Arrange
+
+    //var x = new window.expensesApp.ExpenseReportViewModel();
+    
+
+    // Act
+
+    // Assert
+    ok(1 == "1", "Passed!");
+});
